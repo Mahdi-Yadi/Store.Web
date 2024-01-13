86ebea5da3b9ee76631b9903a2788c9dd59f4941
Mahdi Yadi <mytiyadi@gmail.com>
2024-01-13 14:40:58 +0330

    Add project files.

---
+++ Store.Web/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Store.Web.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Store.Web/Program.cs
@@ -0,0 +1,16 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseStaticFiles();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
